098fd892675d139a2cad93db76ee939304fbfb42
yogeshtavethiya <yogesh.it.kumar@gmail.com>
2024-11-06 23:50:01 +0530

    input added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { PlaceholdersAndVanishInputDemo } from "@/components/changingInput"
+
 export default function Home() {
   return (
     <div className="h-screen w-full bg-neutral-700 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl p-4">
-        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-300  text-center font-sans font-bold">
-          briefsea
-        </h1>
+        <PlaceholdersAndVanishInputDemo />
+        {/* <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-300  text-center font-sans font-bold">
+          BS
+        </h1> */}
       </div>
     </div>
   )
---
+++ src/components/changingInput.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { PlaceholdersAndVanishInput } from "./ui/placeholders-and-vanish-input";
+
+export function PlaceholdersAndVanishInputDemo() {
+  const placeholders = [
+    "Website",
+    "Design",
+    "Advertising",
+    "Content",
+    "Application",
+  ];
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+  return (
+    <div className="h-[40rem] flex flex-col justify-center  items-center px-4">
+      <h2 className="mb-10 sm:mb-20 text-xl text-center sm:text-5xl dark:text-white text-black">
+        Ask Aceternity UI Anything
+      </h2>
+      <PlaceholdersAndVanishInput
+        placeholders={placeholders}
+        onChange={handleChange}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
+}
