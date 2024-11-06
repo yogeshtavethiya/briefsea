b1842d8d532732e08897f0ea9125f1c33ed8fecd
yogeshtavethiya <yogesh.it.kumar@gmail.com>
2024-11-06 21:52:09 +0530

    dashboard init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <div className="flex flex-auto">
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="h-screen w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-2xl p-4">
+        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+          briefsea
+        </h1>
+        <p></p>
+      </div>
     </div>
   )
 }
